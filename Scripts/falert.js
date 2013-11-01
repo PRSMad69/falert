9f5424736d6ce8a0638c658ac7d48f7a7aaa382a
Gaz Franklin <gfranklin@farnborough.ac.uk>
2013-11-01 15:14:28 +0000

    Demo page

---
+++ Scripts/falert.js
@@ -0,0 +1,47 @@
+﻿function falert(message, confirmButtonText, cancelButtonText, confirmCallback) {
+
+    var $container = $('<div class="falert-container"></div>');
+    var $messageContainer = $('<div class="falert-messageContainer"></div>');
+    var $message = $('<p></p>');
+    var $buttonContainer = $('<div class="falert-buttonContainer"></div>');
+    var $button = $('<button class="falert-btn falert-confirm"></button>');
+    var $cancelButton = $('<button class="falert-btn" style="margin-left:5px;"></button>');
+    var $overlay = $('<div class="falert-overlay"></div>');
+    
+
+    $message.html(message);
+
+    $button.text(confirmButtonText);
+    $messageContainer.append($message);
+    $buttonContainer.append($button);
+    $container.append($messageContainer).append($buttonContainer);
+
+    if (cancelButtonText !== undefined && cancelButtonText !== "" && cancelButtonText !== null ) {
+        $cancelButton.text(cancelButtonText);
+        $buttonContainer.append($cancelButton);
+    }
+
+
+    $('body').append($overlay).append($container);
+
+
+
+    $button.click(function() {
+        $container.remove();
+        $overlay.remove();
+        
+        if (confirmCallback !== undefined) {
+            confirmCallback();
+        }
+
+        
+    });
+    
+    $cancelButton.click(function () {
+        $container.remove();
+        $overlay.remove();
+    });
+
+    $button.focus();
+
+}
